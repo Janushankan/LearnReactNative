4525fd9e9e24b6428afca4b6caa9be728e5fd4ac
Janushankan <vj.janushankan1006@gmail.com>
2024-04-09 21:19:56 +0530

    FlexBox, AliginItems, AliginContent, FlexWrap

--- App.js
+++ App.js
@@ -1,118 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { FlatList, Image, ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
 
-  const [months, setMonths] = useState([
-    {name: 'January', id: 1},
-    {name: 'February', id: 2},
-    {name: 'March', id: 3},
-    {name: 'April', id: 4},
-    {name: 'May', id: 5},
-    {name: 'June', id: 6},
-    {name: 'July', id: 7},
-    {name: 'August', id: 8},
-    {name: 'September', id: 9},
-    {name: 'October', id: 10},
-    {name: 'November', id: 11},
-    {name: 'December', id: 12},
-  ])
-
   return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" /> */}
-
-      {/* <textInput style={styles.textInput} placeholder="Enter your name" /> */}
-
-      {/* <Image source={require('./assets/000.jpg')} style={styles.image} resizeMode='cover' />
-      <Image source={{ uri: 'https://picsum.photos/200' }} style={styles.image} resizeMode='center' />
-      <Image
-        style={styles.logo}
-        source={{
-          uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
-        }}
-      /> */}
-
-      {/* <ImageBackground
-        source={require('./assets/janu.jpg')}
-        style={styles.bgImage}
-      >
-        <Text style={styles.textHome}>Hii... Janushankan</Text>
-      </ImageBackground> */}
-
-      {/* <ScrollView>
-        {
-          months.map((month) => {
-            return (
-              <Text key={month.id} style={styles.textScrollView}>{month.name}</Text>
-            )
-          })
-        }
-      </ScrollView> */}
-
-      <FlatList
-        data = {months}
-        // horizontal
-        numColumns={2}
-        keyExtractor = {(item) => item.id}
-        renderItem = {({item}) => (
-          <Text style={styles.textScrollView}>{item.name}</Text>
-        )}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+        <View style={styles.outerView}>
+            <View style={styles.one}><Text>One</Text></View>
+            <View style={styles.two}><Text>Two</Text></View>
+            <View style={styles.three}><Text>Three</Text></View>
+        </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 50,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingTop: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    one: {
+        // flex: 1,
+        width: 50,
+        height: 50,    // 150 -> while overflow
+        backgroundColor: 'red',
+    },
+    two: {
+        // flex: 2,
+        width: 50,
+        height: 50,    // 150 -> while overflow
+        // alignSelf: 'flex-end', // flex-start, flex-end, center, stretch, baseline
+        backgroundColor: 'yellow',
+    },
+    three: {
+        // flex: 3,
+        width: 50,
+        height: 50,    // 150 -> while overflow
+        backgroundColor: 'green',
+    },
+    outerView: {
+        width: 250,
+        height: 250,
+        top: 20,
+        position: 'absolute', // relative, absolute, fixed
+        flexDirection: 'column',    // column, column-reverse, row, row-reverse
+        // justifyContent: 'center',   // flex-start, flex-end, center, space-between, space-around, space-evenly
+        // alignItems: 'center',       // flex-start, flex-end, center, stretch[while using width, height -> not working], baseline
+        backgroundColor: 'pink',
 
-  // textInput: {
-  //   height: 40,
-  //   borderColor: 'gray',
-  //   borderWidth: 1,
-  //   width: 200,
-  //   margin: 10,
-  //   padding: 10,
-  // },
-
-  // image: {
-  //   width: 100,
-  //   height: 100,
-  //   borderWidth: '1',
-  //   borderColor: 'gray',
-  // },
-
-  // bgImage: {
-  //   flex: 1,
-  //   width: '100%',
-  //   height: '100%',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
-
-  // textHome: {
-  //   backgroundColor: 'yellow',
-  //   color: 'white',
-  //   fontSize: 18,
-  //   fontWeight: 'bold',
-  // }
-
-  textScrollView: {
-    fontWeight: 'bold',
-    backgroundColor: 'yellow',
-    color: 'red',
-    width: 150,
-    // height: 80,
-    padding: 15,
-    margin: 15,
-    borderRadius: 10,
-  }
+        // while overflown
+        // flexWrap: 'wrap',   // wrap, nowrap, wrap-reverse
+        // alignContent: 'center', // flex-start, flex-end, center, space-between, space-around, stretch
+    },
 });
